64ac483c65470f0b126e82a66f269862d6f3b961
javiergs <3814755+javiergs@users.noreply.github.com>
2024-10-05 23:24:13 -0700

    JavaDoc Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# biblos
-Reusable packages for homework and projects developed by the academic team
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"javiergs","l":"Genius"},{"p":"javiergs","l":"Paper"}];updateSearchResults();
