69c3045830075bb4711d38af1d82de56c24edee2
Javier Gonzalez <3814755+javiergs@users.noreply.github.com>
2025-02-18 14:44:22 -0800

    Update Face.java

--- DAO/Face.java
+++ DAO/Face.java
@@ -6,12 +6,6 @@ import org.apache.log4j.Logger;
 
 import java.math.BigDecimal;
 
-/**
- * A wrapper for facial expressions data
- *
- * @author javiergs
- * @version 2.0, 2024-07-28
- */
 public class Face extends Signal {
 	
 	private static final Logger logger = LogManager.getLogger(Face.class);
